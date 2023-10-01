7b1dddd4b74eb8bf35427dc6aae1fc19033064c1
Gustavo Fuzer <gustavofuzer256@gmail.com>
2023-10-01 16:23:20 -0300

    feat:router finished and component folder has created

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/index.tsx";
+import { GlobalStyles } from "./styles/GlobalStyle.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    <GlobalStyles />
+  </React.StrictMode>
+);
